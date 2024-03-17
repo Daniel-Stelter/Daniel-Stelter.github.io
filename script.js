86245e104dbddc728c3e851ed675012c60b4a8d0
Daniel Stelter <danielstelter@t-online.de>
2024-03-17 10:50:54 +0100

    Added (buggy) script for collapsing navbar when tapping on item

---
+++ script.js
@@ -0,0 +1,9 @@
+// collapse menu on item selection
+const navLinks = document.querySelectorAll('.nav-item:not(.dropdown)');
+const menuToggle = document.getElementById('navbarSupportedContent');
+const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => {
+        if (menuToggle.classList.contains('show')) { bsCollapse.hide(); }
+    });
+});
